7567719fa2c71b280f06d09719c723ae3efe0a7c
josevagula <jose.gomes.nm.2023@gmail.com>
2026-08-07 22:08:09 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Saas-Cardapio-Digital---001/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
